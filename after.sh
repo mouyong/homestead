59d4ab3d43733b75e3c062f7cdaa2dd7e1bc7e1c
牟勇 <my24251325@gmail.com>
2017-10-09 14:27:22 +0800

    添加自定义配置

--- after.sh
+++ after.sh
@@ -7,15 +7,18 @@
 #sudo apt update
 #sudo apt upgrade -y
 
-pip install --upgrade pip
-pip install shadowsocks
+#pip install --upgrade pip
+#pip install shadowsocks
 
 #sudo sed -i 's/cleanup/reset/g' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
 #sed -in 's/cleanup/reset/gp' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
 
-sudo sslocal -s server -k jiandanai -d restart
+#sudo sslocal -s server -k jiandanai -d restart
+
+sudo sed -in '/try_files/i\\t\tif (!-f $request_filename) {\n\t\trewrite ^(.*)$ /index.php?_rp_=$1 last;\n\t\tbreak;\n}' /etc/nginx/sites-enabled/www.in.app
 
 sed -in '/proxy/'d ~/.bashrc
-sed -in '$a\export all_proxy=http://127.0.0.1:1080' ~/.bashrc
+#sed -in '$a\export all_proxy=http://127.0.0.1:1080' ~/.bashrc
 
-composer config -g repo.packagist composer https://packagist.laravel-china.org && composer global require laravel/installer
+composer config -g repo.packagist composer https://packagist.laravel-china.org 
+# composer global require laravel/installer
